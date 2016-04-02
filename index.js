a2b049d81b2e5ab8e139d6fda8d2ad3e918e8024
Alan Shum <alan200994@gmail.com>
2016-04-02 18:30:20 +0800

    Back to normal mode; crawlling's working

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const config = require('./config.json');
 const crawl = require('./mod/crawler');
 const model = require('./mod/model');
 
-// crawl.recursiveExtractLink(config.rootURL, (page) => {
-// 	model.indexTable.page.upsert(page);
-// 	model.indexTable.word.upsert(Object.keys(page.wordFreq));
-// }, (allPages) => {
-// 	model.file.cleanFile(config.resultFile);
-// 	model.file.writeAll(config.resultFile, allPages);
-// });
+crawl.recursiveExtractLink(config.rootURL, (page) => {
+	model.indexTable.page.upsert(page);
+	model.indexTable.word.upsert(Object.keys(page.wordFreq));
+}, (allPages) => {
+	model.file.cleanFile(config.resultFile);
+	model.file.writeAll(config.resultFile, allPages);
+});
 
 // TODO: Ivan : Check here for API use case
 // model.indexTable.word.getAllID().then((wordIDList) => {
