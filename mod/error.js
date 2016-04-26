d5f0d03c3d8c015266674338f5d1769c9b1c6887
Alan Shum <alan200994@gmail.com>
2016-04-27 00:33:24 +0800

    known error won't stop resolve

--- mod/error.js
+++ mod/error.js
@@ -3,9 +3,10 @@
  */
 module.exports.mongo = {};
 
-module.exports.mongo.parse = (err, cb) => {
+module.exports.mongo.parse = (err, cb, ok_cb) => {
   if (err.code === 11000) {
     console.error(`DUPLICATE ITEM WITH KEY: ${err.errmsg.match(/"(.+)"/)[1]}`);
+    ok_cb && ok_cb(); // known response, continue
   } else {
     cb && cb();
   }
